bbdbe46adaca4e8743932f550a16973dee783482
xiongguoliang <502996316@qq.com>
2024-03-29 00:49:21 +0800

    测试git

--- src/views/hobby/hobbyManager.vue
+++ src/views/hobby/hobbyManager.vue
@@ -50,7 +50,7 @@
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
-                <el-button @click="outerVisible = false">取 消</el-button>
+                <el-button @click="outerVisible = false">取消</el-button>
                 <el-button type="primary" @click="addHobbyForm()">确认</el-button>
             </div>
         </el-dialog>
